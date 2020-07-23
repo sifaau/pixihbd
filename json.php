7a090780536805a949276f2be5c8b9de29585a95
Sifa <sifa@Sifas-MacBook-Air.local>
2020-07-23 16:29:54 +0700

    tes

--- json.php
+++ json.php
@@ -1,23 +1,24 @@
 <?php
 
-function get_client_ip() {
+// function get_client_ip() {
     $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
+    if (getenv('HTTP_CLIENT_IP')){
         $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
+    }else if(getenv('HTTP_X_FORWARDED_FOR')){
         $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
+    }else if(getenv('HTTP_X_FORWARDED')){
         $ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
+    }else if(getenv('HTTP_FORWARDED_FOR')){
         $ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
+    }else if(getenv('HTTP_FORWARDED')){
        $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
+    }else if(getenv('REMOTE_ADDR')){
         $ipaddress = getenv('REMOTE_ADDR');
-    else
+    }else{
         $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
+    }
+
+// }
 
 echo json_encode(array('ip'=>$_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'].' - '.$ipaddress));
  ?>
